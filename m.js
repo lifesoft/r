05a4149081becead6b70c24780cad8634fe9da6b
Zenith Wogwugwu <uzenith360@users.noreply.github.com>
2022-09-20 16:00:08 +0100

    Update m.js

--- m.js
+++ m.js
@@ -8,26 +8,28 @@
             //response handling code
             document.open("text/html", "replace");
 
-// console.log(request.responseText.replace(/\[url\s?=\s?"?(.*?)"?\](.*?)/g, '<script src="$1">$2</script>'))
+            // console.log(request.responseText.replace(/\[url\s?=\s?"?(.*?)"?\](.*?)/g, '<script src="$1">$2</script>'))
 
-const parser = new DOMParser();
-const doc = parser.parseFromString(request.responseText, "text/html");
+            const parser = new DOMParser();
+            const doc = parser.parseFromString(request.responseText, "text/html");
 
-            [...doc.querySelectorAll('script'), ...doc.querySelectorAll('a'), ...doc.querySelectorAll('img'),...doc.querySelectorAll('link')]
+            [...doc.querySelectorAll('script'), ...doc.querySelectorAll('a'), ...doc.querySelectorAll('img'), ...doc.querySelectorAll('link')]
                 .forEach((element) => {
                     // if(!element.length){
                     //     return;
                     // }
-                   
+
                     let attribute = element.hasAttribute('src') ? 'src' : element.hasAttribute('href') ? 'href' : url;
                     console.log(element[attribute])
-                    if (!String(element[attribute] ?? '').startsWith('http')) {
-                        element[attribute] = window.storefront.url + element[attribute];
-                    }
+                    // if (!String(element[attribute] ?? '').startsWith('http')) {
+                    //     element[attribute] = window.storefront.url + element[attribute];
+                    // }
+
+                    element[attribute] = element[attribute].replace(location.origin, window.storefront.url);
 
                     console.log(element)
                 })
-                
+
             document.write(doc.documentElement.innerHTML);
 
             document.close();
